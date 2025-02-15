18517b574879ccec1543379d4080871471b7d8ab
tzachi81 <tzahih@gmail.com>
2025-02-15 22:24:38 +0200

    fix build capital css file name

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { Home } from "./routes/home_route/Home";
 import { Show } from "./routes/Show";
-import "./app.css";
+import "./App.css";
 
 interface Props {}
 
